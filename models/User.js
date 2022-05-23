d7019b0488e90c3306d4a050fb886b30366bfb3f
mateonav98 <mateonav98@gmail.com>
2022-05-23 16:45:36 -0700

    adding User schema

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// User Schema
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
+
+// creating virtual to receive friend count
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model('user', userSchema)
+module.exports = User
