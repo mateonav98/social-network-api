46861521bbc68aa2b4c86ff8027c0c28bc39b738
mateonav98 <mateonav98@gmail.com>
2022-05-23 17:18:09 -0700

    adding thoughts schema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -28,3 +28,31 @@ const reactionSchema = new Schema(
         }
     }
 );
+
+const thoughtsSchema = new Schema(
+    {
+      thoughtsText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
