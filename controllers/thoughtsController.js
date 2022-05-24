28198bbe544bcb08e872e1a111fdedd793d4e166
mateonav98 <mateonav98@gmail.com>
2022-05-23 21:22:53 -0700

    get and create thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,13 +1,43 @@
-const { User, Thoughts } = require("../models");
+const { Thoughts, User } = require("../models");
 
 module.exports = {
-    getThoughts() {
-
+    getThoughts(req, res) {
+        Thoughts.find({})
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then(thoughtData => res.json(thoughtData))
+        .catch((err) => res.status(500).json(err));
     },
-    getThoughtsById(){
-
+    getThoughtsById(req, res) {
+        Thoughts.findOne({ _id: req.params.id })
+          .populate({ path: "reactions", select: "-__v"})
+          .select("-__v")
+          .then((thoughtData) => {
+            if (!thoughtData) {
+              res.status(404).json({ message: "No thoughts found." });
+              return;
+            }
+            res.json(thoughtData);
+          })
+          .catch((err) => res.status(500).json(err));
     },
-    createThoughts() {
+    createThoughts(req, res) {
+        Thoughts.create(req.body)
+          .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: req.body.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then((thoughtData) => {
+            if (!thoughtData) {
+              res.status(404).json({ message: "No user found." });
+              return;
+            }
+            res.json(thoughtData);
+          })
+          .catch((err) => res.status(500).json(err));
 
     },
     updateThoughts() {
@@ -20,7 +50,7 @@ module.exports = {
 
     },
     deleteReaction() {
-        
+
     }
 
 }
